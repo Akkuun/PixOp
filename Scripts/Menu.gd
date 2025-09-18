b7295880485c863a9add74340ea46826c725728d
Akkuun <mathisduban@gmail.com>
2025-09-18 17:15:55 +0200

    ¨sceneWIP¨

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -3,12 +3,13 @@ extends Node
 @export var buttonPlay: Button
 @export var buttonQuit: Button
 
+
+
 func _ready():
+	
+	
 	buttonPlay.pressed.connect(_on_play_pressed)
 	buttonQuit.pressed.connect(_on_quit_pressed)
-	
-	
-
 
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://Scenes/mainScene2D.tscn")
---
+++ Scripts/imageStack.gd
@@ -0,0 +1,26 @@
+extends Control
+
+var images := [
+	preload("res://ressources/Image/file.svg"),
+	preload("res://ressources/Image/file.svg"),
+	preload("res://ressources/Image/file.svg"),
+]
+
+func _ready():
+	for tex in images:
+		var rect = TextureRect.new()
+		rect.texture = tex
+		rect.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
+		
+		# Centrer les images sur le Control parent
+		rect.anchor_left = 0.5
+		rect.anchor_top = 0.5
+		rect.anchor_right = 0.5
+		rect.anchor_bottom = 0.5
+		rect.position = Vector2.ZERO  
+		rect.pivot_offset = rect.size / 2  # pivot au centre de l'image
+		
+		# Rotation aléatoire légère (-10° à +10°)
+		rect.rotation_degrees = randf_range(-10.0, 10.0)
+		
+		add_child(rect)
