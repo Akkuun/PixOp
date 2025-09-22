59a3c628cd9d93233e73fe29817cb9958ad0f6e9
Mathis Duban <mathisduban@gmail.com>
2025-09-22 09:06:53 +0200

    UX fixed

--- Scripts/imageStack.gd
+++ Scripts/imageStack.gd
@@ -13,12 +13,13 @@ func _ready():
 		rect.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
 		
 		# Centrer les images sur le Control parent
-		rect.anchor_left = 0.5
-		rect.anchor_top = 0.5
-		rect.anchor_right = 0.5
-		rect.anchor_bottom = 0.5
+		#rect.anchor_left = 0.5
+		#rect.anchor_top = 0.5
+		#rect.anchor_right = 0.5
+		#rect.anchor_bottom = 0.5
 		rect.position = Vector2.ZERO  
 		rect.pivot_offset = rect.size / 2  # pivot au centre de l'image
+		rect.scale = Vector2(0.7,0.7)
 		
 		# Rotation aléatoire légère (-10° à +10°)
 		rect.rotation_degrees = randf_range(-10.0, 10.0)
