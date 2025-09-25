e5c81dbbe165141055c1f2e15cb06ca8ed649091
akkuun <mathisduban@gmail.com>
2025-09-25 10:23:44 +0200

    ¨GraphNode1x3¨

--- Scripts/Graph/addCompressionNode.gd
+++ Scripts/Graph/addCompressionNode.gd
@@ -1,17 +1,17 @@
 extends Button
 
-@export var node_scene: PackedScene
+@export var prefabNode: PackedScene
 @export var graph_edit: GraphEdit
 
 func _ready():
 	pressed.connect(_on_button_pressed)
 
 func _on_button_pressed():
-	if node_scene == null or graph_edit == null:
+	if prefabNode == null or graph_edit == null:
 		push_error("node_scene ou graph_edit non assigné")
 		return
 	
-	var new_node: GraphNode = node_scene.instantiate()
+	var new_node: GraphNode = prefabNode.instantiate()
 	
 	# Positionner au centre du GraphEdit
 	var offset = graph_edit.scroll_offset + graph_edit.size / 2
