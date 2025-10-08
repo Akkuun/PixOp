d3e65fd4e30e4150b00fe6138933130bd33c83e1
akkuun <mathisduban@gmail.com>
2025-10-08 11:09:54 +0200

    ¨sound¨

--- Scripts/Graph/GraphEditor.gd
+++ Scripts/Graph/GraphEditor.gd
@@ -1,16 +1,24 @@
 extends GraphEdit
 
 
+@export var connectionSound: AudioStream 
 
+var audio_player: AudioStreamPlayer
 
 func _ready():
 	# Connexions pour créer / supprimer les liens
 	connection_request.connect(_on_connection_request)
 	disconnection_request.connect(_on_disconnection_request)
 
+	audio_player = AudioStreamPlayer.new()
+	audio_player.stream = connectionSound
+	add_child(audio_player)
+
 func _on_connection_request(from_node: StringName, from_port: int, to_node: StringName, to_port: int):
 	if isConnectionValid(from_node, from_port, to_node, to_port):
 		connect_node(from_node, from_port, to_node, to_port)
+		audio_player.play()
+
 
 func _on_disconnection_request(from_node: StringName, from_port: int, to_node: StringName, to_port: int):
 	disconnect_node(from_node, from_port, to_node, to_port)
