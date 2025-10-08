b2e4975d3f77b188e72d3066452015d61c0c6077
akkuun <mathisduban@gmail.com>
2025-10-08 10:58:05 +0200

    ¨PreventMultiConnectionOnEnterSlot¨

--- Scripts/Graph/GraphEditor.gd
+++ Scripts/Graph/GraphEditor.gd
@@ -1,5 +1,8 @@
 extends GraphEdit
 
+
+
+
 func _ready():
 	# Connexions pour créer / supprimer les liens
 	connection_request.connect(_on_connection_request)
@@ -14,6 +17,10 @@ func _on_disconnection_request(from_node: StringName, from_port: int, to_node: S
 
 
 func isConnectionValid(from_node: StringName, from_port: int, to_node: StringName, to_port: int) -> bool:
-	if from_node == to_node:  # Si le noeud de départ est le même que le noeud d'arrivée
+	if from_node == to_node:  # Si le noeud de départ est le même que le noeud d'arrivée donc on tente une connexion sur sa propre entrée, on refuse la connexion
 		return false
+	# On empeche de connecter plusieurs fois une entrée
+	for conn in get_connection_list():
+		if conn["to_node"] == to_node and conn["to_port"] == to_port:
+			return false
 	return true
