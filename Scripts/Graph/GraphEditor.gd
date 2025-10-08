461ae90d9bca5011180525d95698a96714cc27e5
akkuun <mathisduban@gmail.com>
2025-10-08 10:39:14 +0200

    ¨PreventConnectionExitEnterOnSameNode¨

--- Scripts/Graph/GraphEditor.gd
+++ Scripts/Graph/GraphEditor.gd
@@ -6,7 +6,14 @@ func _ready():
 	disconnection_request.connect(_on_disconnection_request)
 
 func _on_connection_request(from_node: StringName, from_port: int, to_node: StringName, to_port: int):
-	connect_node(from_node, from_port, to_node, to_port)
+	if isConnectionValid(from_node, from_port, to_node, to_port):
+		connect_node(from_node, from_port, to_node, to_port)
 
 func _on_disconnection_request(from_node: StringName, from_port: int, to_node: StringName, to_port: int):
 	disconnect_node(from_node, from_port, to_node, to_port)
+
+
+func isConnectionValid(from_node: StringName, from_port: int, to_node: StringName, to_port: int) -> bool:
+	if from_node == to_node:  # Si le noeud de départ est le même que le noeud d'arrivée
+		return false
+	return true
